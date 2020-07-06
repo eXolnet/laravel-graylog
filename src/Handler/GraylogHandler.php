b3410c9ae6193f74cc61a41cd72d32256245a524
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2020-07-06 12:34:55 -0400

    Add support for TCP and HTTP transports

--- src/Handler/GraylogHandler.php
+++ src/Handler/GraylogHandler.php
@@ -5,7 +5,11 @@ namespace Exolnet\Graylog\Handler;
 use Exolnet\Graylog\Processor\ExtraProcessor;
 use Exolnet\Graylog\Processor\LaravelProcessor;
 use Gelf\Publisher;
+use Gelf\Transport\HttpTransport;
+use Gelf\Transport\SslOptions;
+use Gelf\Transport\TcpTransport;
 use Gelf\Transport\UdpTransport;
+use InvalidArgumentException;
 use Monolog\Handler\GelfHandler;
 use Monolog\Logger;
 use Monolog\Processor\IntrospectionProcessor;
@@ -16,14 +20,24 @@ use Monolog\Processor\WebProcessor;
 class GraylogHandler extends GelfHandler
 {
     /**
+     * @param string $transport
+     * @param bool   $secure
      * @param string $host
-     * @param int $port
-     * @param int $level
-     * @param array $extra
+     * @param int    $port
+     * @param string $path
+     * @param int    $level
+     * @param array  $extra
      */
-    public function __construct(string $host, int $port, $level = Logger::NOTICE, array $extra = [])
-    {
-        $transport = new UdpTransport($host, $port);
+    public function __construct(
+        string $transport,
+        bool $secure,
+        string $host,
+        int $port,
+        string $path,
+        $level = Logger::NOTICE,
+        array $extra = []
+    ) {
+        $transport = self::makeTransport($transport, $secure, $host, $port, $path);
         $publisher = new Publisher($transport);
 
         parent::__construct($publisher, $level, true);
@@ -35,5 +49,29 @@ class GraylogHandler extends GelfHandler
         $this->pushProcessor(new WebProcessor());
         $this->pushProcessor(new MemoryUsageProcessor());
         $this->pushProcessor(new MemoryPeakUsageProcessor());
+    }
+
+    /**
+     * @param string $transport
+     * @param bool   $secure
+     * @param string $host
+     * @param int    $port
+     * @param string $path
+     * @return \Gelf\Transport\TransportInterface
+     * @throws \InvalidArgumentException
+     */
+    protected static function makeTransport(string $transport, bool $secure, string $host, int $port, string $path)
+    {
+        if ($transport === 'udp') {
+            return new UdpTransport($host, $port);
+        } elseif ($transport === 'tcp') {
+            return new TcpTransport($host, $port, ($secure) ? new SslOptions() : null);
+        } elseif ($transport === 'http') {
+            return new HttpTransport($host, $port, $path, ($secure) ? new SslOptions() : null);
+        } elseif ($transport === 'https') {
+            return new HttpTransport($host, $port, $path, new SslOptions());
+        } else {
+            throw new InvalidArgumentException("Transport [{$transport}] is not supported.");
+        }
     }
 }
