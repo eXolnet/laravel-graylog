eab492d913176e8906f133fe4e3ec60410f3e503
Simon Gaudreau <sgaudreau@exolnet.com>
2021-09-28 08:35:25 -0400

    Instead of using the Log Facade directly in the service provider, use a call after resolving on LogManager

--- src/GraylogServiceProvider.php
+++ src/GraylogServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Exolnet\Graylog;
 
 use Exolnet\Graylog\Handler\GraylogHandler;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Formatter\GelfMessageFormatter;
@@ -14,24 +15,26 @@ class GraylogServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Log::extend('graylog', function ($app, array $config) {
-            $defaultConfig = [
-                'handler' => GraylogHandler::class,
-                'handler_with' => [
-                    'transport' => 'udp',
-                    'host' => 'localhost',
-                    'port' => 12201,
-                    'path' => '/gelf',
-                    'extra' => [
-                        //
+        $this->callAfterResolving(LogManager::class, function (LogManager $log) {
+            $log->extend('graylog', function ($app, array $config) {
+                $defaultConfig = [
+                    'handler' => GraylogHandler::class,
+                    'handler_with' => [
+                        'transport' => 'udp',
+                        'host' => 'localhost',
+                        'port' => 12201,
+                        'path' => '/gelf',
+                        'extra' => [
+                            //
+                        ],
                     ],
-                ],
-                'formatter' => GelfMessageFormatter::class,
-            ];
+                    'formatter' => GelfMessageFormatter::class,
+                ];
 
-            $config = array_replace_recursive($defaultConfig, $config);
+                $config = array_replace_recursive($defaultConfig, $config);
 
-            return $this->createMonologDriver($config);
+                return $this->createMonologDriver($config);
+            });
         });
     }
 }
