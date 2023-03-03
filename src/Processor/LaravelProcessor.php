5571092d4d6a9510812556c818a3710fe7221148
Bruno Lapierre <blapierre@exolnet.com>
2023-03-03 15:13:15 -0500

    chore(deps): add monolog 3 support

--- src/Processor/LaravelProcessor.php
+++ src/Processor/LaravelProcessor.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Str;
+use Monolog\LogRecord;
 use Monolog\Processor\ProcessorInterface;
 
 class LaravelProcessor implements ProcessorInterface
@@ -13,7 +14,7 @@ class LaravelProcessor implements ProcessorInterface
     /**
      * {@inheritDoc}
      */
-    public function __invoke(array $record)
+    public function __invoke(array|LogRecord $record)
     {
         $record['extra']['level_name'] = $record['level_name'];
         $record['extra']['app'] = Str::slug(Config::get('app.name'));
